f01a8d99872709c0d23d938471411ca3d3618055
BlessingsMinga <113256890+BlessingsMinga@users.noreply.github.com>
2023-06-09 00:10:17 +0200

    Add files via upload

---
+++ BEEP.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <windows.h>
+ 
+// Driver Code
+int main()
+{
+ 
+    // Function that beeps a sound of
+    // frequency 750 for 0.8 sec
+    Beep(750, 800);
+ 
+    
+    return 0;
+}
