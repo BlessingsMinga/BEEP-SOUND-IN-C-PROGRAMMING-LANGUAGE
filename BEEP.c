04e708ca4f3df34e0159b431172c8effc7165070
BlessingsMinga <113256890+BlessingsMinga@users.noreply.github.com>
2023-06-16 03:35:45 +0200

    BEEP.c

--- BEEP.c
+++ BEEP.c
@@ -1,13 +1,1 @@
 
-#include <stdio.h>
-#include <windows.h>
-int main()
-{
- 
-    // Function that beeps a sound of
-    // frequency 750 for 0.8 sec
-    Beep(750, 800);
- 
-    
-    return 0;
-}
